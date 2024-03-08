9145e0ae805e132eeec6f5db05ed6063b42afb1c
Yugank Anchal <90154802+Yugankanchal@users.noreply.github.com>
2024-03-08 20:42:48 +0000

    adding the todo to our GUI

--- TodoList_Vanilajs/todoLsit.js
+++ TodoList_Vanilajs/todoLsit.js
@@ -6,16 +6,27 @@ function getValue() {
     return value;
 }
 
-// function updateList() {
-//     const listItems = document.getElementById('list-items');
-//     for (let i = 0; i < TodoList.length; i++) {
-
-//     }
-// }
-function addTodo() {
-    if (getValue() != '') {
-        TodoList.push(getValue());
-        // updateList();
+function renderList(lastIndex) {
+    const listItems = document.getElementById('list-items');
+    for (let i = lastIndex; i < TodoList.length; i++) {
+        const listElement = document.createElement('li');
+        listElement.innerHTML = `<i id="bullet" class="fa-regular fa-circle"></i> <span>${TodoList[i]}</span> <i id="cross" class="fa-regular fa-circle-xmark"></i>`;
+        console.log(listElement);
+        listItems.append(listElement);
     }
-    console.log(TodoList);
+    document.getElementById('Total-todo').innerText = TodoList.length;
 }
+function addTodo() {
+    const todoValue = getValue();
+    if (todoValue != '') {
+        TodoList.push(todoValue)
+    }
+    renderList(TodoList.length - 1);
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        addTodo();
+    }
+
+})
